767563589e379ae3d8b79e74df2a39b69acbbe30
Diogo Monica <diogo@squareup.com>
2014-12-24 12:40:51 +0000

    Add users to seeds; change default passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,41 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
 
+
+# Criar utilizadores de exemplo
+
+User.find_or_create_by!(email: 'diogo@example.com') do |user|
+        user.password = 'change'
+        user.password_confirmation = 'change'
+        user.confirm!
+end
+
+User.find_or_create_by!(email: 'nuno@example.com') do |user|
+        user.password = 'change'
+        user.password_confirmation = 'change'
+        user.confirm!
+end
+
+User.find_or_create_by!(email: 'paulo@example.com') do |user|
+        user.password = 'change'
+        user.password_confirmation = 'change'
+        user.confirm!
+end
+
+
+# Criar organizacoes de exemplo
 mrcc = Organization.create(:name => "MRCC", :country => "Portugal")
 comar = Organization.create(:name => "COMAR", :country => "Portugal")
 mrccn = Organization.create(:name => "MRCC", :country => "Nigeria")
 fake = Organization.create(:name => "FakeOrg", :country => "Brazil")
 
+# Criar situacoes de exemplo
 primeira = Situation.create(:name => "Homem ao mar", :description => "Esta e nacional")
 segunda = Situation.create(:name => "Poluicao no guincho", :description => "Esta e local mrcc")
 terceira = Situation.create(:name => "Homicidio no guincho", :description => "Esta internacional")
 quarta = Situation.create(:name => "Terrorismo no guincho")
 
+# Criar participacoes de exemplo
 primeira.organizations << [mrcc, comar]
 segunda.organizations << [mrcc]
 terceira.organizations << [mrccn, comar]
