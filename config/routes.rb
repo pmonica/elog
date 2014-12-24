587201881e5377702fb6c67f23f53291581f0d25
Diogo Monica <diogo@squareup.com>
2014-12-24 17:22:00 +0000

    Changed redirect on signup to have instructions on confirmation link

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :organizations
 
   root to: 'dashboards#index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "registrations" }
+
   resources :users
 end
